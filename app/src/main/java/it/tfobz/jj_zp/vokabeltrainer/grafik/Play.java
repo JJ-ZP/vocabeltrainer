0b450b0cb833f7f9ef3e659ad7e309f2f25df3b9
Jonas Zöschg <jon.zoe.z@gmail.com>
2017-11-12 13:02:35 +0100

    added new layouts

---
+++ app/src/main/java/it/tfobz/jj_zp/vokabeltrainer/grafik/Play.java
@@ -0,0 +1,15 @@
+package it.tfobz.jj_zp.vokabeltrainer.grafik;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import it.tfobz.jj_zp.vokabeltrainer.R;
+
+public class Play extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+    }
+}
